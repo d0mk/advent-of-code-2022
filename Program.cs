d0274d0828295b96bb63362a78b271616100a1da
Dominik <dominik.kobyra@gmail.com>
2022-12-03 23:05:57 +0100

    added a reflection based algorithm to discover solutions for all days so far, execute them and print the results

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
-﻿namespace AdventOfCode;
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
 
 public class Program
 {
     public static void Main()
     {
-        var solutionContext = new Day1.Solution(@"Day1/input.txt");
+        // just an ugly expression which gives back a list of namespaces in ascending
+        // order by comparing the day number (namespaces are like AdventOfCode.DayX)
+        var groupedNamespaces = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .GroupBy(t => t.Namespace)
+            .Where(n => n.Key.Contains("Day"))
+            .OrderBy(x => Convert.ToInt32(Regex.Match(x.Key, @"Day(\d+)").Groups[1].Value));
 
-        var part1Result = solutionContext.GetSolutionPart1();
-        var part2Result = solutionContext.GetSolutionPart2();
+        int dayNumber = 1;
 
-        Console.WriteLine($"Part 1: {part1Result}");
-        Console.WriteLine($"Part 2: {part2Result}");
+        // also an ugly foreach which iterates over all solutions,
+        // executes them and prints the results
+        foreach (var typesInNamespace in groupedNamespaces)
+        {
+            var solutionType = typesInNamespace.FirstOrDefault(x => Regex.Match(x.FullName, @"AdventOfCode\.Day\d+\.Solution$").Success);
+
+            var solutionInstance = Activator.CreateInstance(
+                solutionType,
+                $"Day{dayNumber}/input.txt");
+
+            dynamic solutionContext = Convert.ChangeType(solutionInstance, solutionType);
+
+            var part1Result = solutionContext.GetSolutionPart1();
+            var part2Result = solutionContext.GetSolutionPart2();
+
+            Console.WriteLine($"Day {dayNumber++}");
+            Console.WriteLine($"* Part 1: {part1Result}\n* Part 2: {part2Result}\n");
+        }
     }
 }
