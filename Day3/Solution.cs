c1b1f35e7d5eee9039e0fb8d1101a487f2d4b390
Dominik <dominik.kobyra@gmail.com>
2022-12-03 23:34:13 +0100

    added solution for day 3 part 1

---
+++ Day3/Solution.cs
@@ -0,0 +1,55 @@
+using AdventOfCode.Base;
+
+namespace AdventOfCode.Day3;
+
+public class Solution : BaseSolution<int, int>
+{
+    private readonly Dictionary<char, int> priorities = new();
+
+    public Solution(string inputPath)
+    {
+        InitializeData(inputPath);
+    }
+
+    protected override void InitializeData(string inputPath)
+    {
+        fileContent = File.ReadAllLines(inputPath);
+
+        int priority = 1;
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            priorities.Add(c, priority++);
+        }
+
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            priorities.Add(c, priority++);
+        }
+    }
+
+    public override int GetSolutionPart1()
+    {
+        int sumOfPriorities = 0;
+
+        foreach (var line in fileContent)
+        {
+            var firstHalf = line.Substring(0, line.Length / 2);
+            var secondHalf = line.Substring(line.Length / 2, line.Length / 2);
+
+            var commonElements = Enumerable.Intersect(firstHalf, secondHalf);
+
+            foreach (var item in commonElements)
+            {
+                sumOfPriorities += priorities[item];
+            }
+        }
+
+        return sumOfPriorities;
+    }
+
+    public override int GetSolutionPart2()
+    {
+        return 0;
+    }
+}
