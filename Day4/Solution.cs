3f69d4efdc26b1b020931610042cc533ca991185
Dominik <dominik.kobyra@gmail.com>
2022-12-04 13:41:11 +0100

    added solution for day 4 part 1

---
+++ Day4/Solution.cs
@@ -0,0 +1,72 @@
+using AdventOfCode.Base;
+
+namespace AdventOfCode.Day4;
+
+struct Pair
+{
+    public Pair(int left, int right)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    public Pair(string left, string right)
+    {
+        Left = Convert.ToInt32(left);
+        Right = Convert.ToInt32(right);
+    }
+
+    public int Left { get; }
+
+    public int Right { get; }
+
+    public bool IsContainedBy(Pair other)
+    {
+        return Left >= other.Left && Right <= other.Right;
+    }
+}
+
+public class Solution : BaseSolution<int, int>
+{
+    private readonly List<(Pair, Pair)> pairs = new();
+
+    public Solution(string inputPath) : base(inputPath)
+    {
+        InitializeData();
+    }
+
+    protected override void InitializeData()
+    {
+        foreach (var line in fileContent)
+        {
+            var bothPairs = line.Split(",");
+            var splitLeftPair = bothPairs[0].Split("-");
+            var splitRightPair = bothPairs[1].Split("-");
+
+            Pair leftPair = new Pair(splitLeftPair[0], splitLeftPair[1]);
+            Pair rightPair = new Pair(splitRightPair[0], splitRightPair[1]);
+
+            pairs.Add((leftPair, rightPair));
+        }
+    }
+
+    public override int GetSolutionPart1()
+    {
+        int count = 0;
+
+        foreach (var (leftPair, rightPair) in pairs)
+        {
+            if (leftPair.IsContainedBy(rightPair) || rightPair.IsContainedBy(leftPair))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public override int GetSolutionPart2()
+    {
+        return 1;
+    }
+}
